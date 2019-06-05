4cb31b7b5acfbc79d9656764eec5403f15b79c5c
unknown <rajibkhan976@gmail.com>
2019-06-05 15:59:04 +0200

    delete function and add student form implemented

--- src/components/DashboardComponent.js
+++ src/components/DashboardComponent.js
@@ -33,28 +33,121 @@ class DashboardComponent extends Component {
     })
   }
 
+  deleteStudentById = (studentId, e) => {
+    fetch("http://localhost:5000/student/" + studentId, {
+      method: 'DELETE',
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin': 'http://localhost:3000/',
+        'Access-Control-Allow-Credentials': 'true',
+        'Content-Type': 'application/json'
+      }
+    }).then(() => {
+      alert('Student deleted successfully');
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  handleId = (e) => {
+    this.studentId = e.target.value;
+  }
+
+  handleName = (e) => {
+    this.studentName = e.target.value;
+  }
+
+  handleEmail = (e) => {
+    this.studentEmail = e.target.value;
+  }
+
+  handleRoad = (e) => {
+    this.studentRoad = e.target.value;
+  }
+
+  handleZipcode = (e) => {
+    this.studentZipcode = e.target.value;
+    alert(this.studentZipcode);
+  }
+
+  handleCity = (e) => {
+    this.studentCity = e.target.value;
+  }
+
+  addStudent = (e) => {
+    alert(this.studentCity);
+  }
+
   render () {
     let listOfStudents = this.state.students;
     console.log(this.state.students);
     return (
       <div className="container">
         <div className="row">
-          <div className="col-6">
+          <div className="col-12">
             <h2>Bong-bong</h2>
-            <ul className="list-group">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Student id</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Address</th>
+                  <th scope="col">Delete</th>
+                </tr>
+              </thead>
+              <tbody>
               {listOfStudents.map((studentData, index) => {
-                return <div key={index}>
-                        <li className="list-group-item">
-                          <h5>Student id: {studentData.students._id}</h5>
-                          <p>Name: {studentData.students.name}</p>
-                          <p>Email: {studentData.students.email}</p>
-                        </li>
-                       </div>;
+                return <tr key={index}>
+                        <th scope="row">{studentData.students._id}</th>
+                        <td>{studentData.students.name}</td>
+                        <td>{studentData.students.email}</td>
+                        <td>
+                        {studentData.students.address.road}, &nbsp;
+                        {studentData.students.address.zipcode}, &nbsp;
+                        {studentData.students.address.city}
+                        </td>
+                        <td><button type="button" className="btn btn-danger" onClick={(e) => this.deleteStudentById(studentData._id, e)}>Delete</button></td>
+                       </tr>;
               })}
-            </ul>
+              </tbody>
+            </table>
           </div>
         </div>
-      </div>
+        <div className="row">
+          <div className="col-6">
+              <div className="jumbotron">
+                <h3>Add student</h3>
+                <div className="form-group">
+                  <label className="float-left" for="studentId">Student id</label>
+                  <input id="studentId" className="form-control" type="text" onChange= {this.handleId}/>
+                </div>
+                <div className="form-group">
+                  <label className="float-left" for="studentName">Name</label>
+                  <input id="studentName" className="form-control" type="text" onChange= {this.handleName}/>
+                </div>
+                <div className="form-group">
+                  <label className="float-left" for="studentEmail">Email</label>
+                  <input id="studentEmail" className="form-control" type="email" onChange= {this.handleEmail}/>
+                </div>
+                <h5>Address</h5>
+                <div className="form-group">
+                  <label className="float-left" for="studentRoad">Road</label>
+                  <input id="studentRoad" className="form-control" type="email" onChange= {this.handleRoad}/>
+                </div>
+                <div className="form-group">
+                  <label className="float-left" for="studentZipcode">Zip code</label>
+                  <input id="studentZipcode" className="form-control" type="email" onChange= {this.handleZipcode}/>
+                </div>
+                <div className="form-group">
+                  <label className="float-left" for="studentCity">City</label>
+                  <input id="studentCity" className="form-control" type="email" onChange= {this.handleCity}/>
+                </div> <br/>
+                <button type="button" class="btn btn-success" onClick={this.addStudent}>Add student</button>
+             </div>
+           </div>
+         </div>
+       </div>
     );
   }
 }
